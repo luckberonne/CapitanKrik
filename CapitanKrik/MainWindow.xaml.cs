a60e5e5601efca240770ef4f08164002039ec79b
luckberonne <luckberonne@gmail.com>
2021-01-01 22:18:35 -0300

    Agregar archivos de proyecto.

---
+++ CapitanKrik/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CapitanKrik
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            // Set tooltip visibility
+
+            if (Tg_Btn.IsChecked == true)
+            {
+                tt_subir.Visibility = Visibility.Collapsed;
+                tt_renombrar.Visibility = Visibility.Collapsed;
+                tt_backup.Visibility = Visibility.Collapsed;
+                tt_reiniciar.Visibility = Visibility.Collapsed;
+                tt_configuracion.Visibility = Visibility.Collapsed;
+                tt_log.Visibility = Visibility.Collapsed;
+                tt_cerrar.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_subir.Visibility = Visibility.Visible;
+                tt_renombrar.Visibility = Visibility.Visible;
+                tt_backup.Visibility = Visibility.Visible;
+                tt_reiniciar.Visibility = Visibility.Visible;
+                tt_configuracion.Visibility = Visibility.Visible;
+                tt_log.Visibility = Visibility.Visible;
+                tt_cerrar.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Tg_Btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+
+        private void Tg_Btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Tg_Btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
